7844b4ab8d8e935678399958a1a9ae3b3b182d51
Tyler Sloan <tcsloan44@gmail.com>
2024-07-17 23:37:08 -0400

    started adding routes and working on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import StandardGameBoard from './components/StandardGameBoard.jsx'
+import HomePage from './components/HomePage.jsx'
 
 function App() {
 
   return (
     <>
-      <StandardGameBoard />
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/standard_game' element={<StandardGameBoard/>}/>
+      </Routes>
     </>
   )
 }
