69462fe52cca3ef2c202f2f0cb089ae5b25ba969
Tyler Sloan <tcsloan44@gmail.com>
2024-07-23 22:11:12 -0400

    finished functionality of entering specifically 1 letter into 1 tile and it automatically moving to the next tile and created TileRow component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import StandardGameBoard from './components/StandardGameBoard.jsx'
+import StandardGameBoard from './components/GameBoard/StandardGameBoard.jsx'
 import HomePage from './components/HomePage.jsx'
 // need to add element to timed_game mode
 function App() {
