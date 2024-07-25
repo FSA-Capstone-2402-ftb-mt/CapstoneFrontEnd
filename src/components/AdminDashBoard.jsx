9e828e26b04d151cde11948a920ceaff3e1a7185
EdwinVichesGarcia <edwinviches@gmail.com>
2024-07-24 17:44:37 -0700

    AdminDashBoard Folder with its components

--- src/components/AdminDashBoard.jsx
+++ src/components/AdminDashBoard.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-
+import { Route, Routes } from 'react-router-dom';
+import AdminAppBar from './AdminComponents/AppBar';
+import WordTable from './AdminComponents/WordTab';
+import UserTable from './UserTab';
 
 export default function AdminDashBoard() {
 
@@ -7,7 +10,12 @@ export default function AdminDashBoard() {
   return (
     <>
       <h1>Admin Dashboard</h1>
-     
+      <AdminAppBar/>
+      <WordTable/>
+      <UserTable/>
+
+     <Routes> 
+     </Routes>
     </>
   );
 }
---
+++ src/components/UserTab.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Button } from "@mui/material";
+
+function createData(id, username, join_date, overall_score, overall_games) {
+  return {id, username, join_date, overall_score, overall_games };
+}
+
+const rows = [
+  createData(1,'AlbertoM',0 , 0, 0),
+  createData(2,'TylerS', 0, 0, 0),
+  createData(3,'EdwinV', 0, 0, 0),
+  createData(4,'SlavikT', 0, 0, 0),
+];
+
+export default function UserTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Users</TableCell>
+            <TableCell align="right">ID</TableCell>
+            <TableCell align="right">Username</TableCell>
+            <TableCell align="right">Join Date</TableCell>
+            <TableCell align="right">Overall Score</TableCell>
+            <TableCell align="right">Overall Games</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.id}</TableCell>
+              <TableCell align="right">{row.username}</TableCell>
+              <TableCell align="right">{row.join_date}</TableCell>
+              <TableCell align="right">{row.overall_score}</TableCell>
+              <TableCell align="right">{row.overall_games}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Button
+      style={{
+        backgroundColor: 'lightblue',
+        borderRadius:'25px',
+
+      }}>See All Users</Button>
+    </TableContainer>
+    
+  );
+}
